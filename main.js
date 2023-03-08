54cf899349543f0e657ecd7d56008739844e2a3b
Mancska <95695531+Mancska@users.noreply.github.com>
2023-03-08 09:34:35 +0100

    coomit

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-window.addEventListener("load",init)
-function init(){
-    const articleElem=document.querySelector("article")
-    articleElem.innerHTML="<button>lets do it!!!</button>"
+window.addEventListener("load", init);
+function init() {
+  const articleElem = document.querySelector("article");
+  articleElem.innerHTML += "<button>lets do it!!!</button>";
+  kep();
+}
+function gombkattintas() {
+  console.log("valami");
+}
+function kep() {
+  const articleElem = document.querySelectorAll("article img");
 
+  lebeg[0].src="DALL·E 2023-03-08 09.16.08 - Red Dragon  with huge wings , cyberpunk _.png" alt="sárkány">;
+  const lebeg = document.querySelector("#kepp");
+  lebeg[0].addEventListener("mouseover", function () {
+    lebeg[0].style.border = "1px solid green";
+    lebeg[0].class.add("formazasClass");
+  });
 }
